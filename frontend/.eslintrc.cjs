7319be7bad10ad32d4ad65f30fbcced5d2658e65
elianiva <dicha.arkana03@gmail.com>
2022-07-25 20:32:23 +0700

    chore(frontend): setup eslint and prettier

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,33 @@
+/**
+ * @type {import('eslint').Linter.Config}
+ */
+module.exports = {
+	extends: ["kentcdodds", "kentcdodds/react", "kentcdodds/jsx-a11y"],
+	settings: {
+		"import/resolver": {
+			alias: {
+				extensions: [".js", ".jsx", ".ts", ".tsx"],
+				map: [["~", "./src"]],
+			},
+		},
+	},
+	rules: {
+		"@typescript-eslint/explicit-function-return-type": ["off"],
+		"@typescript-eslint/unbound-method": "off",
+		"@typescript-eslint/no-unused-vars": [
+			"warn",
+			{ argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+		],
+		"import/prefer-default-export": "off",
+		"no-console": "warn",
+		"no-restricted-imports": ["error", { patterns: ["~/features/*/*"] }],
+		"no-warning-comments": "off",
+		radix: ["error", "as-needed"],
+		"react/react-in-jsx-scope": "off",
+		"react/jsx-filename-extension": [
+			"error",
+			{ extensions: [".jsx", ".tsx"] },
+		],
+		"react/jsx-sort-props": ["warn", { reservedFirst: ["key"] }],
+	},
+};
---
+++ frontend/.prettierrc.cjs
@@ -0,0 +1,14 @@
+/**
+ * @type {import('prettier').Config}
+ */
+module.exports = {
+	arrowParens: "always",
+	bracketSpacing: true,
+	endOfLine: "auto",
+	printWidth: 120,
+	semi: true,
+	singleQuote: false,
+	jsxSingleQuote: false,
+	trailingComma: "es5",
+	useTabs: true,
+};
